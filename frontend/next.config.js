89ec6a9c695641d920776d39c63d183a738023d0
Asher Winstead <mrashcreates@gmail.com>
2024-09-27 11:51:30 -0500

    update

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,3 +1,8 @@
+import dynamic from 'next/dynamic';
+
+// Example of a component being dynamically loaded
+const HeavyComponent = dynamic(() => import('../components/HeavyComponent'));
+
 const withPWA = require('next-pwa')({
   dest: 'public',
 });
@@ -27,3 +32,18 @@ const nextConfig = {
 };
 
 module.exports = withPlugins([withPWA], nextConfig);
+module.exports = {
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      // Disable splitting for the server build
+      config.optimization.splitChunks = false;
+
+      // Ensure the Webpack pack files are smaller by setting a reasonable limit
+      config.performance = {
+        maxAssetSize: 512000, // 500 KB for individual assets
+        maxEntrypointSize: 512000,
+      };
+    }
+    return config;
+  },
+};
