2005eb956110607b2d2190d2b59da2d0d66751cf
Asher Winstead <mrashcreates@gmail.com>
2024-09-27 01:40:20 -0500

    alpha 1.0.0

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,12 +1,8 @@
 // frontend/next.config.js
-/** @type {import('next').NextConfig} */
 const withPWA = require('next-pwa')({
-  dest: 'public',
-  disable: process.env.NODE_ENV === 'development',
-});
-
-const nextConfig = {
-  // Your existing Next.js config options here
-};
-
-module.exports = withPWA(nextConfig);
+    dest: 'public',
+  });
+  
+  module.exports = withPWA({
+    // next.js config
+  });
