58897e52700271323879240e2d4813a7708fc430
Asher Winstead <mrashcreates@gmail.com>
2024-09-27 11:12:02 -0500

    alpha 1.0.0

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -11,3 +11,23 @@ const nextConfig = {
 const composedPlugins = withPlugins([withPWA], nextConfig);
 
 module.exports = composedPlugins;
+module.exports = {
+  experimental: {
+    modularizeImports: {
+      lodash: {
+        transform: 'lodash/{{member}}',
+      },
+    },
+  },
+};
+module.exports = {
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.optimization.splitChunks = false;
+    }
+    return config;
+  },
+};
+module.exports = {
+  compress: true,
+};
