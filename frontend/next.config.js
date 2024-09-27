dfabaf1a7573d81a4ccf1671366cef3168ac7b40
Asher Winstead <mrashcreates@gmail.com>
2024-09-27 11:04:52 -0500

    alpha 1.0.0 fixed some hopefully unneeded things

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,26 +1,13 @@
-// frontend/next.config.js
-
 const withPWA = require('next-pwa')({
   dest: 'public',
-  // Additional PWA options if needed
 });
 
 const { withPlugins } = require('next-compose-plugins');
-const { withCloudflarePagesAdapter } = require('@cloudflare/next-on-pages');
 
 const nextConfig = {
   reactStrictMode: true,
-  // Other Next.js configuration options
 };
 
-// Compose your plugins
-const composedPlugins = withPlugins(
-  [
-    withPWA,
-    // Add other plugins here if necessary
-  ],
-  nextConfig
-);
+const composedPlugins = withPlugins([withPWA], nextConfig);
 
-// Export the configuration wrapped with the Cloudflare Pages adapter
-module.exports = withCloudflarePagesAdapter(composedPlugins);
+module.exports = composedPlugins;
