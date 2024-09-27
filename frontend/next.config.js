1a15de3371216781a93455f954ab01e3d028f1bc
Asher Winstead <mrashcreates@gmail.com>
2024-09-27 02:12:37 -0500

    fix v2

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,8 +1,21 @@
 // frontend/next.config.js
+
 const withPWA = require('next-pwa')({
-    dest: 'public',
-  });
-  
-  module.exports = withPWA({
-    // next.js config
-  });
+  dest: 'public',
+  // Additional PWA options if needed
+});
+
+const withCloudflarePages = require('@cloudflare/next-on-pages/with-cloudflare');
+const { withPlugins } = require('next-compose-plugins');
+
+module.exports = withPlugins(
+  [
+    withCloudflarePages,
+    withPWA,
+    // Add other plugins here if necessary
+  ],
+  {
+    reactStrictMode: true,
+    // Other Next.js configuration options
+  }
+);
